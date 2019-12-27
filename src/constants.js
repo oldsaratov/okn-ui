d39fb2daa2a01efebb3cb33e4b49c4cf8cb4bb95
Maria Druzina <druzina.mary@gmail.com>
2019-12-27 22:47:00 +0100

    Add Search and Type fields to List page

---
+++ src/constants.js
@@ -0,0 +1,10 @@
+export const BASE_URL = 'https://dev.okn.oldsaratov.ru/api';
+
+export const PAGE_SIZE = 20;
+
+export const OBJECT_OPTIONS = [
+    { label: 'Федеральный', value: 1 },
+    { label: 'Региональный', value: 2 },
+    { label: 'Муниципальный', value: 3 },
+    { label: 'Выявленный', value: 4 }
+];
--- src/pages/List.js
+++ src/pages/List.js
@@ -1,13 +1,47 @@
 import React from 'react';
+import { Icon, Input, Select } from 'antd';
+
+import { OBJECT_OPTIONS } from '../constants';
+import './List.css';
+
+const { Option } = Select;
 
 class List extends React.Component {
+
+    onSearchTermChange = ({ target: { value } }) => {
+        console.log('onSearchTermChange: ', value); // TODO: Call service
+    };
+
+    onTypeChange = value => {
+        console.log('onTypeChange: ', value); // TODO: Call service
+    };
 
     render() {
         return (
             <div>
-                <h2>List</h2>
+                <Input
+                    placeholder="Поиск"
+                    prefix={<Icon type="search"/>}
+                    onPressEnter={this.onSearchTermChange}
+                    allowClear
+                    className="okn-search-box"
+                />
+
+                <Select
+                    mode="multiple"
+                    placeholder="Тип"
+                    showArrow={true}
+                    onChange={this.onTypeChange}
+                    className="okn-type-select"
+                >
+                    {this.renderSelectOptions()}
+                </Select>
             </div>
         );
+    }
+
+    renderSelectOptions() {
+        return OBJECT_OPTIONS.map(opt => <Option key={opt.value}>{opt.label}</Option>);
     }
 }
 
