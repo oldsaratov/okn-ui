5f05e20f060bfc201720334072230c4dac38c211
Maria Druzina <druzina.mary@gmail.com>
2019-12-30 12:52:30 +0100

    Set Redux DevTools

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import { ConfigProvider } from 'antd';
 import ruRu from 'antd/es/locale/ru_RU';
@@ -11,7 +11,11 @@ import reducers from './reducers';
 
 import './index.css';
 
-const store = createStore(reducers, applyMiddleware(thunk));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+    reducers,
+    composeEnhancers(applyMiddleware(thunk))
+);
 
 ReactDOM.render(
     <Provider store={store}>
