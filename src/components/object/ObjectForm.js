5d0cbe71bd87cd9008b86bc365e55f8b568feede
mdruzina <druzina.mary@gmail.com>
2020-01-10 14:45:10 +0100

    Move ObjectFrom to separate file

---
+++ src/components/object/ObjectForm.js
@@ -0,0 +1,96 @@
+import React, { Component } from 'react';
+import { Button, Col, Form, Input, Row, Select } from 'antd';
+
+import Upload from '../Upload';
+import { OBJECT_TYPES } from '../../constants';
+import './ObjectForm.scss';
+
+const { TextArea } = Input;
+const { Option } = Select;
+
+const requiredFieldRule = { required: true, message: 'Обязательное поле' };
+
+const ObjectForm = Form.create({
+    name: 'object_form',
+    mapPropsToFields(props) {
+        const { object } = props;
+
+        return (object && {
+            name: Form.createFormField({ ...object.name, value: object.name }),
+            description: Form.createFormField({ ...object.description, value: object.description }),
+            type: Form.createFormField({ ...object.type, value: object.type }),
+            photos: Form.createFormField({ ...object.photos, value: object.photos })
+        }) || {};
+    },
+    onValuesChange(props, changedFields) {
+        props.onChange(changedFields);
+    }
+})(class extends Component {
+
+    handleSubmit = event => {
+        event.preventDefault();
+
+        this.props.form.validateFields((err, values) => {
+            if (err) {
+                return;
+            }
+
+            this.props.onSubmit(values);
+        });
+    };
+
+    hasErrors = fieldsError => {
+        return Object.keys(fieldsError).some(field => fieldsError[field]);
+    };
+
+    render() {
+        const { form, loading } = this.props;
+        const { getFieldDecorator, getFieldsError } = form;
+
+        return (
+            <Form colon={false} layout="vertical" onSubmit={this.handleSubmit} className="okn-object-form">
+                <Form.Item label="Название">
+                    {getFieldDecorator('name', { rules: [requiredFieldRule] })(<Input/>)}
+                </Form.Item>
+
+                <Row gutter={32}>
+                    <Col span={12}>
+                        <Form.Item label="Фото" className="okn-object-form__photos">
+                            {getFieldDecorator('photos', {
+                                valuePropName: 'fileList'
+                            })(<Upload type="image"/>)}
+                        </Form.Item>
+                    </Col>
+                    <Col span={12}>
+                        <Form.Item label="Описание">
+                            {getFieldDecorator('description')(<TextArea autoSize={{ minRows: 10, maxRows: 20 }}/>)}
+                        </Form.Item>
+
+                        <Form.Item label="Тип">
+                            {getFieldDecorator('type')(
+                                <Select placeholder="Тип">
+                                    {OBJECT_TYPES.map(opt => <Option key={opt.value} value={opt.value}>{opt.label}</Option>)}
+                                </Select>
+                            )}
+                        </Form.Item>
+                    </Col>
+                </Row>
+
+                <Row type="flex" justify="end">
+                    <Button onClick={() => this.props.onCancel()}>Отмена</Button>
+                    <Button
+                        type="primary"
+                        htmlType="submit"
+                        disabled={this.hasErrors(getFieldsError())}
+                        loading={loading}
+                        className="okn-object-form__submit"
+                    >
+                        Сохранить
+                    </Button>
+                </Row>
+            </Form>
+        );
+    }
+});
+
+export default ObjectForm;
--- src/pages/ObjectEdit.js
+++ src/pages/ObjectEdit.js
@@ -1,102 +1,12 @@
 import React, { Component, Fragment } from 'react';
 import { connect } from 'react-redux';
-import { Button, Col, Form, Input, Row, Select, Spin } from 'antd';
+import { Spin } from 'antd';
 
 import { fetchObject, updateObject } from '../actions';
 import ObjectEvents from '../components/events/Events';
-import Upload from '../components/Upload';
-import { OBJECT_TYPES } from '../constants';
+import ObjectForm from '../components/object/ObjectForm';
 import history from '../history';
 import { getActionStatus } from '../selectors';
-
-const { TextArea } = Input;
-const { Option } = Select;
-
-const requiredFieldRule = { required: true, message: 'Обязательное поле' };
-
-const ObjectForm = Form.create({
-    name: 'object_form',
-    mapPropsToFields(props) {
-        const { object } = props;
-
-        return (object && {
-            name: Form.createFormField({ ...object.name, value: object.name }),
-            description: Form.createFormField({ ...object.description, value: object.description }),
-            type: Form.createFormField({ ...object.type, value: object.type }),
-            photos: Form.createFormField({ ...object.photos, value: object.photos })
-        }) || {};
-    },
-    onValuesChange(props, changedFields) {
-        props.onChange(changedFields);
-    }
-})(class extends Component {
-
-    handleSubmit = event => {
-        event.preventDefault();
-
-        this.props.form.validateFields((err, values) => {
-            if (err) {
-                return;
-            }
-
-            this.props.onSubmit(values);
-        });
-    };
-
-    hasErrors = fieldsError => {
-        return Object.keys(fieldsError).some(field => fieldsError[field]);
-    };
-
-    render() {
-        const { form, loading } = this.props;
-        const { getFieldDecorator, getFieldsError } = form;
-
-        return (
-            <Form colon={false} layout="vertical" onSubmit={this.handleSubmit} className="okn-object-form">
-                <Form.Item label="Название">
-                    {getFieldDecorator('name', { rules: [requiredFieldRule] })(<Input/>)}
-                </Form.Item>
-
-                <Row gutter={32}>
-                    <Col span={12}>
-                        <Form.Item label="Фото" className="okn-object-form__photos">
-                            {getFieldDecorator('photos', {
-                                valuePropName: 'fileList'
-                            })(<Upload type="image"/>)}
-                        </Form.Item>
-                    </Col>
-                    <Col span={12}>
-                        <Form.Item label="Описание">
-                            {getFieldDecorator('description')(<TextArea autoSize={{ minRows: 10, maxRows: 20 }}/>)}
-                        </Form.Item>
-
-                        <Form.Item label="Тип">
-                            {getFieldDecorator('type')(
-                                <Select placeholder="Тип">
-                                    {OBJECT_TYPES.map(opt => <Option key={opt.value}
-                                                                     value={opt.value}>{opt.label}</Option>)}
-                                </Select>
-                            )}
-                        </Form.Item>
-                    </Col>
-                </Row>
-
-                <Row type="flex" justify="end">
-                    <Button onClick={() => this.props.onCancel()}>Отмена</Button>
-                    <Button
-                        type="primary"
-                        htmlType="submit"
-                        disabled={this.hasErrors(getFieldsError())}
-                        loading={loading}
-                        className="okn-object-form__submit"
-                    >
-                        Сохранить
-                    </Button>
-                </Row>
-            </Form>
-        );
-    }
-});
 
 class ObjectEdit extends Component {
     state = { formObject: null };
