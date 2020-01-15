538033ff0a17277c14a9bd0ed8c5ef825e442bbc
Maria Druzina <druzina.mary@gmail.com>
2020-01-15 23:16:30 +0100

    Home: Switch between map and list

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Icon } from 'antd';
+import { Col, Icon, Radio, Row } from 'antd';
 
 import { fetchAllObjects } from '../actions';
 import './Home.scss';
@@ -14,8 +14,19 @@ class Home extends React.Component {
     render() {
         return (
             <div className="okn-home-page">
+                <Row gutter={16} type="flex" justify="space-between"className="okn-filters-panel">
+                    <Col></Col>
+
+                    <Col>
+                        <Radio.Group defaultValue="map" buttonStyle="solid" className="okn-filters-panel__type">
+                            <Radio.Button value="map"><Icon type="picture"/></Radio.Button>
+                            <Radio.Button value="list"><Icon type="unordered-list"/></Radio.Button>
+                        </Radio.Group>
+                    </Col>
+                </Row>
+
                 <div className="okn-empty-state okn-empty-state--grey">
-                    <Icon type="hourglass" className="okn-empty-state__icon" />
+                    <Icon type="hourglass" className="okn-empty-state__icon"/>
                     <h2>{this.props.objects.length} объектов когда-нибудь будут показаны здесь на карте</h2>
                 </div>
             </div>
