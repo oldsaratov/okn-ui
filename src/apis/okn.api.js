dee5dfaf2743a2f17654156c4cf28fd322f463d9
Maria Druzina <druzina.mary@gmail.com>
2020-02-23 15:48:24 +0100

    Add API base url to env config

--- src/apis/okn.api.js
+++ src/apis/okn.api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { authService } from '../services/auth.service';
 
 export default axios.create({
-    baseURL: 'https://dev.okn.oldsaratov.ru/api',
+    baseURL: process.env.REACT_APP_API_BASE_URL,
     headers: {
         'Authorization': `Bearer ${authService.getAccessToken()}`
     }
