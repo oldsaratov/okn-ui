23d015f04ee061be4b409a419ebeb3208c107647
Maria Druzina <druzina.mary@gmail.com>
2020-03-03 23:14:34 +0100

    Show "Add Object" button only to logged in users

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -4,13 +4,14 @@ import { Button, Col, Row } from 'antd';
 
 import history from '../../history';
 import { VIEW_TYPES } from '../../constants';
+import { authService } from '../../services/auth.service';
 import ObjectList from './ObjectList';
 import ObjectMap from './ObjectMap';
 import FiltersPanel from '../../components/FiltersPanel';
 import './Home.scss';
 
 const Home = (props) => {
-    const { filters } = props;
+    const { isLoggedIn, filters } = props;
     const showMap = filters.viewType === VIEW_TYPES.MAP;
 
     return (
@@ -28,19 +29,24 @@ const Home = (props) => {
             <Row>
                 <Col span={16}></Col>
 
-                <Col span={8} className="okn-new-object-section">
-                    <Button
-                        type="primary"
-                        onClick={() => history.push(`/objects/new`)}
-                    >
-                        Добавить объект
-                    </Button>
-                </Col>
+                {isLoggedIn && (
+                    <Col span={8} className="okn-new-object-section">
+                        <Button
+                            type="primary"
+                            onClick={() => history.push(`/objects/new`)}
+                        >
+                            Добавить объект
+                        </Button>
+                    </Col>
+                )}
             </Row>
         </div>
     );
 };
 
-const mapStateToProps = (state) => ({ filters: state.filters });
+const mapStateToProps = (state) => ({
+    isLoggedIn: authService.isLoggedIn(),
+    filters: state.filters
+});
 
 export default connect(mapStateToProps)(Home);
